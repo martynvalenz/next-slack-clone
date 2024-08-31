6efefd85c4355ccc17a07572415540bddd66ad00
martynvalenz <martynvalenz@gmail.com>
2024-08-30 22:18:36 -0600

    feat: Add Sidebar component and update workspace layout

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,9 +1,10 @@
 import { useCurrentMember } from '@/features/members/api/use-current-meber'
 import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace'
 import { useWorkspaceId } from '@/hooks/useWorkspaceId'
-import { AlertTriangle, Loader2 } from 'lucide-react'
+import { AlertTriangle, Loader2, MessageSquareText, SendHorizonal } from 'lucide-react'
 import React from 'react'
 import WorkspaceSidebarHeader from './WorkspaceSidebarHeader'
+import SidebarItem from './SidebarItem'
 
 const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId()
@@ -26,6 +27,18 @@ const WorkspaceSidebar = () => {
   return (
     <div className='flex flex-col bg-[#5e2c5f] h-full'>
       <WorkspaceSidebarHeader workspace={workspace} isAdmin={member.role == 'admin'}/>
+      <div className='flex flex-col px-2 mt-3'>
+        <SidebarItem
+          label="Threads"
+          icon={MessageSquareText}
+          id="threads"
+        />
+        <SidebarItem
+          label="Drafts & Sent"
+          icon={SendHorizonal}
+          id="drafts"
+        />
+      </div>
     </div>
   )
 }
