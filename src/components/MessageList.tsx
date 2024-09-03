1264eb8028c9bf72d014b40a9e6ed875a19a63d3
martynvalenz <martynvalenz@gmail.com>
2024-09-02 22:10:48 -0600

    feat: Add ChannelHero component for displaying channel information
    - Created a new component ChannelHero to display the name and creation time of a channel.
    - Used the ChannelHero component in the MessageList component to show the channel information.

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,6 +1,11 @@
 import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
 import { differenceInMinutes, format, isToday, isYesterday } from "date-fns";
 import Message from "./Message";
+import ChannelHero from "./ChannelHero";
+import { useState } from "react";
+import { Id } from "../../convex/_generated/dataModel";
+import { useWorkspaceId } from "@/hooks/useWorkspaceId";
+import { useCurrentMember } from "@/features/members/api/use-current-meber";
 
 const TIME_THRESHOLD = 5
 
@@ -38,6 +43,11 @@ const MessageList = ({
   isLoadingMore,
   canLoadMore,
 }:MessageListProps) => {
+  const [editingId,setEditingId] = useState<Id<'messages'> | null>(null)
+  const workspaceId = useWorkspaceId()
+
+  const {data:currentMember} = useCurrentMember({workspaceId})
+
   const groupedMessages = data?.reduce(
     (groups,message) => {
       const date = new Date(message._creationTime)
@@ -77,16 +87,16 @@ const MessageList = ({
                     memberId={message.memberId}
                     authorImage={message.user.image}
                     authorName={message.user.name}
-                    isAuthor={false}
+                    isAuthor={message.memberId === currentMember?._id}
                     reactions={message.reactions}
                     body={message.body}
                     image={message.image}
                     updatedAt={message.updatedAt}
                     createdAt={message._creationTime}
-                    isEditing={false}
-                    setEditingId={() => {}}
+                    isEditing={editingId === message._id}
+                    setEditingId={setEditingId}
                     isCompact={isCompact}
-                    hideThreadButton={false}
+                    hideThreadButton={variant === 'thread'}
                     threadCount={message.threadCount}
                     threadImage={message.threadImage}
                     threadTimestamp={message.threadTimestamp}
@@ -96,6 +106,14 @@ const MessageList = ({
             }
           </div>
         ))
+      }
+      {
+        variant === 'channel' && channelName && channelCreationTime && (
+          <ChannelHero
+            name={channelName}
+            creationTime={channelCreationTime}
+          />
+        )
       }
     </div>
   )
