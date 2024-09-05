f88c2b304fcb678a9c0d13e3e2dd8475422d29d0
martynvalenz <martynvalenz@gmail.com>
2024-09-04 20:25:56 -0600

    feat: Add useRemoveMember and useUpdateMember hooks for member management
    feat: Add useRemoveMember and useUpdateMember hooks for member management

--- convex/members.ts
+++ convex/members.ts
@@ -1,7 +1,7 @@
+import { Id } from './_generated/dataModel.d';
 import { getAuthUserId } from "@convex-dev/auth/server";
-import { query, QueryCtx } from "./_generated/server";
+import { mutation, query, QueryCtx } from "./_generated/server";
 import { v } from "convex/values";
-import { Id } from "./_generated/dataModel";
 
 const populateUser = (ctx:QueryCtx,userId:Id<'users'>) => {
   return ctx.db.get(userId) 
@@ -84,7 +84,97 @@ export const current = query({
       .unique()
 
     if(!member) return null
-
     return member
+  }
+})
+
+export const update = mutation({
+  args:{
+    id:v.id('members'),
+    role:v.union(v.literal('admin'),v.literal('member')),
+  },
+  handler: async(ctx, args) => {
+    const userId = await getAuthUserId(ctx)
+    if(!userId) throw new Error('Unauthorized')
+    
+    const member = await ctx.db.get(args.id)
+    if(!member) throw new Error('Member not found')
+
+    const currentMember = await ctx.db
+      .query('members')
+      .withIndex('by_user_id_and_workspace_id',(q) =>
+        q.eq('userId',userId)
+        .eq('workspaceId',member.workspaceId)
+      )
+      .unique()
+    
+    if(!currentMember) throw new Error('Unauthorized')
+    if(currentMember.role !== 'admin') throw new Error('Unauthorized')
+
+    await ctx.db.patch(args.id,{role:args.role})
+
+    return args.id
+  }
+})
+
+export const remove = mutation({
+  args:{
+    id:v.id('members'),
+  },
+  handler: async(ctx, args) => {
+    const userId = await getAuthUserId(ctx)
+    if(!userId) throw new Error('Unauthorized')
+    
+    const member = await ctx.db.get(args.id)
+    if(!member) throw new Error('Member not found')
+
+    const currentMember = await ctx.db
+      .query('members')
+      .withIndex('by_user_id_and_workspace_id',(q) =>
+        q.eq('userId',userId)
+        .eq('workspaceId',member.workspaceId)
+      )
+      .unique()
+    
+    if(!currentMember) throw new Error('Unauthorized')
+    // if(currentMember.role !== 'admin') throw new Error('Unauthorized')
+
+    if(member.role === 'admin') {
+      throw new Error('Cannot remove admin')
+    }
+
+    if(currentMember._id === args.id && currentMember.role === 'admin') throw new Error('Cannot remove itself, if it is the only admin')
+    
+    const [messages,reactions,conversations] = await Promise.all([
+      ctx.db.query('messages')
+        .withIndex('by_member_id',(q) => 
+          q.eq('memberId',member._id)
+        )
+        .collect(),
+
+      ctx.db.query('reactions')
+        .withIndex('by_member_id',(q) => 
+          q.eq('memberId',member._id)
+        )
+        .collect(),
+      
+      ctx.db.query('conversations')
+        .filter((q) => q.or(
+          q.eq(q.field('memberOneId'),member._id),
+          q.eq(q.field('memberTwoId'),member._id)
+        ))
+        .collect()
+    ])
+    for(const message of messages) {
+      await ctx.db.delete(message._id)
+    }
+    for(const reaction of reactions) {
+      await ctx.db.delete(reaction._id)
+    }
+    for(const conversation of conversations) {
+      await ctx.db.delete(conversation._id)
+    }
+    await ctx.db.delete(args.id)
+    return args.id
   }
 })
--- src/features/members/components/Profile.tsx
+++ src/features/members/components/Profile.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button"
 import { Id } from "../../../../convex/_generated/dataModel"
 import { useGetMember } from "../api/use-get-member"
-import { AlertTriangle, Loader2, MailIcon, XIcon } from "lucide-react"
+import { AlertTriangle, ChevronDownIcon, DeleteIcon, Loader2, MailIcon, XIcon } from "lucide-react"
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
 import { Separator } from "@/components/ui/separator"
 import Link from "next/link"
+import { useUpdateMember } from "../api/use-update-member"
+import { useRemoveMember } from "../api/use-remove-member"
+import { useCurrentMember } from "../api/use-current-meber"
+import { useWorkspaceId } from "@/hooks/useWorkspaceId"
+import { toast } from "sonner"
+import { useConfirm } from "@/hooks/useConfirm"
+import { useRouter } from "next/navigation"
+import { DropdownMenu,DropdownMenuContent,DropdownMenuTrigger, DropdownMenuRadioGroup, DropdownMenuRadioItem } from "@/components/ui/dropdown-menu"
+
 
 interface ProfileProps {
   memberId:Id<'members'>
@@ -15,8 +24,66 @@ const Profile = ({
   memberId,
   onClose
 }:ProfileProps) => {
+  const router = useRouter()
+  const workspaceId = useWorkspaceId()
+  const [LeaveDialog,confirmLeave] = useConfirm(
+    'Leave workspace',
+    'Are you sure you want to leave this workspace?',
+  )
+  const [RemoveDialog,confirmRemove] = useConfirm(
+    'Remove member',
+    'Are you sure you want to remove this member?',
+  )
+  const [UpdateDialog,confirmUpdate] = useConfirm(
+    'Update role',
+    'Are you sure you want to update this member role?',
+  )
   const {data:member,isLoading:loadingMember} = useGetMember({memberId})
+  const {data:currentMember,isLoading:isLoadingCurrentMember} = useCurrentMember({workspaceId})
+  const {mutate:updateMember,isPending:isUpdatingMember} = useUpdateMember()
+  const {mutate:removeMember,isPending:isRemovingMember} = useRemoveMember()
 
+  const onRemove = async() => {
+    const ok = await confirmRemove()
+    if(!ok) return
+    removeMember({id:memberId}, {
+      onSuccess:() => {
+        toast.success('Member removed')
+        onClose()
+      },
+      onError:() => {
+        toast.error('Failed to remove member')
+      }
+    })
+  }
+
+  const onLeave = async() => {
+    const ok = await confirmLeave()
+    if(!ok) return
+    removeMember({id:memberId}, {
+      onSuccess:() => {
+        toast.success('You have left the workspace')
+        router.replace('/')
+        onClose()
+      },
+      onError:() => {
+        toast.error('Failed to leave workspace')
+      }
+    })
+  }
+  
+  const onUpdate = async(role:'admin' | 'member') => {
+    const ok = await confirmUpdate()
+    updateMember({id:memberId,role}, {
+      onSuccess:() => {
+        toast.success('Role updated')
+        onClose()
+      },
+      onError:() => {
+        toast.error('Failed to update role')
+      }
+    })
+  }
 
   if(loadingMember) {
     return (
@@ -52,36 +119,76 @@ const Profile = ({
   }
 
   return (
-    <div className="h-full flex flex-col">
-      <div className="h-[49px] flex justify-between items-center p-2 border-b">
-        <p className="text-lg font-bold">Profile</p>
-        <Button onClick={onClose} size="iconSm" variant="ghost">
-          <XIcon className="size-6" />
-        </Button>
-      </div>
-      <div className="p-4 flex flex-col justify-center items-center">
-        <Avatar className="max-w-[256px] max-h-[256px] size-full">
-          <AvatarImage src={member.user.image} />
-          <AvatarFallback className="text-6xl aspect-square">{member.user.name?.[0]}</AvatarFallback>
-        </Avatar>
-      </div>
-      <div className="flex flex-col p-4">
-        <p className="text-xl font-bold">{member.user.name}</p>
-      </div>
-      <Separator />
-      <div className="flex flex-col p-4">
-        <p className="text-sm font-bold mb-4">Contact information</p>
-        <div className="flex items-center gap-2">
-          <div className="size-9 rounded-md bg-muted flex items-center justify-center">
-            <MailIcon className="size-6" />
-          </div>
-          <div className="flex flex-col">
-            <p className="text-sm font-bold">Email</p>
-            <Link href={`mailto:${member.user.email}`} className="text-sm hover:underline">{member.user.email}</Link>
+    <>
+      <LeaveDialog />
+      <RemoveDialog />
+      <UpdateDialog />
+      <div className="h-full flex flex-col">
+        <div className="h-[49px] flex justify-between items-center p-2 border-b">
+          <p className="text-lg font-bold">Profile</p>
+          <Button onClick={onClose} size="iconSm" variant="ghost">
+            <XIcon className="size-6" />
+          </Button>
+        </div>
+        <div className="p-4 flex flex-col justify-center items-center">
+          <Avatar className="max-w-[256px] max-h-[256px] size-full">
+            <AvatarImage src={member.user.image} />
+            <AvatarFallback className="text-6xl aspect-square">{member.user.name?.[0]}</AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="flex flex-col p-4">
+          <p className="text-xl font-bold">{member.user.name}</p>
+          {
+            (currentMember?.role === 'admin' &&  currentMember?._id !== member._id) ? (
+              <div className="flex items-center gap-2 mt-4">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline" className="w-full capitalize">
+                      {member.role} <ChevronDownIcon className="size-4 ml-2" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-full">
+                    <DropdownMenuRadioGroup
+                      value={member.role}
+                      onValueChange={(value) => onUpdate(value as 'admin' | 'member')}
+                    >
+                      <DropdownMenuRadioItem value="admin">
+                        Admin
+                      </DropdownMenuRadioItem>
+                      <DropdownMenuRadioItem value="member">
+                        Member
+                      </DropdownMenuRadioItem>
+                    </DropdownMenuRadioGroup>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <Button variant="outline" className="w-full" onClick={onRemove}>
+                  Remove
+                </Button>
+              </div>
+            ) : (currentMember?.role !== 'admin' && currentMember?._id === member._id) ? (
+              <div>
+                <Button className="mt-4" onClick={onLeave}>
+                  Leave
+                </Button>
+              </div>
+            ) : null
+          }
+        </div>
+        <Separator />
+        <div className="flex flex-col p-4">
+          <p className="text-sm font-bold mb-4">Contact information</p>
+          <div className="flex items-center gap-2">
+            <div className="size-9 rounded-md bg-muted flex items-center justify-center">
+              <MailIcon className="size-6" />
+            </div>
+            <div className="flex flex-col">
+              <p className="text-sm font-bold">Email</p>
+              <Link href={`mailto:${member.user.email}`} className="text-sm hover:underline">{member.user.email}</Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
